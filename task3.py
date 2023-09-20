7af82506920135780f38d7e2632bfacc6647df14
priyadharshini004 <144123015+priyadharshini004@users.noreply.github.com>
2023-09-20 07:26:51 -0700

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,37 @@
+tamil = int(input("Enter the Tamil mark:"))
+english = int(input("Enter the English mark:"))
+maths = int(input("Enter the Maths mark:"))
+physics = int(input("Enter the Physics mark:"))
+chemistry = int(input("Enter the Chemistry mark:"))
+computer_science = int(input("Enter the Computer science mark:"))
+if(tamil>english and tamil>maths and tamil>physics and tamil>chemistry and tamil>computer_science):
+	if(tamil>100):
+		print("invalid tamil mark")
+	else:
+		print("max mark is",tamil)
+
+elif(english>maths and english>physics and english>chemistry and english>computer_science):
+	if(english>100):
+		print("invalid english mark")
+	else:
+		print("max mark is",english)
+elif(maths>physics and maths>chemistry and maths>computer_science):
+	if(tamil>100):
+		print("invalid maths mark")
+	else:
+		print("max mark is",maths)
+elif(physics>chemistry and physics>computer_science):
+	if(tamil>100):
+		print("invalid physics mark")
+	else:
+		print("max mark is",physics)
+elif(chemistry>computer_science):
+	if(tamil>100):
+		print("invalid chemistry mark")
+	else:
+		print("max mark is",chemistry)
+else:
+	if(tamil>100):
+		print("invalid tamil mark")
+	else:
+	 	print("max mark is",computer_science)
